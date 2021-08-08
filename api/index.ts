6c27ed45fb9ffed45e2fe7d85485f7e674abce2e
xiaoluoboding <xiaoluoboding@gmail.com>
2021-08-08 16:30:46 +0800

    feat: use metascraper scrape websites

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,27 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import got from 'got'
 
-export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  res.send(`Hello ${name}!`)
+const metascraper = require('metascraper')([
+  // require('metascraper-author')(),
+  // require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-logo')(),
+  // require('metascraper-clearbit')(),
+  // require('metascraper-publisher')(),
+  require('metascraper-title')()
+  // require('metascraper-url')()
+])
+
+const scrapeMetaData = async (targetUrl: string) => {
+  const { body: html, url } = await got(targetUrl)
+  const metadata = await metascraper({ html, url })
+  console.log(metadata)
+  return metadata
+}
+
+export default async function handler (req: VercelRequest, res: VercelResponse) {
+  const url = req.query.url as string
+  const metadata = await scrapeMetaData(url)
+  res.json(metadata)
 }
